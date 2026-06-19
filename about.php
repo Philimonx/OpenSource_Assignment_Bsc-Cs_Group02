e5e9195169d897abd499893ce384dc77695cc815
Philimon Gaspar <philimongaspar@gmail.com>
2026-06-19 07:22:53 +0300

    Added about page feature

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About System</title>
+</head>
+<body>
+
+<h2>Student Information Management System</h2>
+
+<p>
+This system helps Tanzanian primary and secondary schools
+manage student information efficiently.
+</p>
+
+</body>
+</html>
